0cd38d3893e7af89ae8635a07217f6acf22cf72a
Vsdestroyer0 <ggmendoza2005@gmail.com>
2025-10-21 01:57:09 -0600

    Dotenv importado y conectado a db.js

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
+// Importaciones de paquetes
+import "dotenv/config";
 import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI;
-
+// Funcion para conectar a la base de datos
 export default async function connectDB() {
-    try {
-        await mongoose.connect(uri);
-        console.log("Conectado a base de datos MongoDB");
-    } catch (error) {
-        console.error("Error conectando a MongoDB:", error);
-        process.exit(1);
-    }
-};
+  try {
+    const uri = process.env.MONGODB_URI;
+    await mongoose.connect(uri);
+    console.log("Conectado a base de datos MongoDB");
+  } catch (error) {
+    console.error("Error conectando a MongoDB:", error);
+    process.exit(1);
+  }
+}
+
+// dotenv config sirve para cargar las variables de entorno, es decir, las variables que se encuentran en el archivo .env
