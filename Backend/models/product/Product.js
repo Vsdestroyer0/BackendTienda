7b545d215b9daab897a0661ae691488245816e49
Emir Tirso <turumbar451@hotmail.com>
2025-11-19 06:21:35 -0600

    se reparo el bug de name en productos
    no tenia name en el modelo xd, se lo agregue y listo, ahora si graba el name en la base de datos

--- Backend/models/product/Product.js
+++ Backend/models/product/Product.js
@@ -16,6 +16,7 @@ const variantSchema = new mongoose.Schema({
 });
 
 const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
     brand: { type: String, required: true },
     category: { type: String, required: true },
     price: { type: Number, required: true },
